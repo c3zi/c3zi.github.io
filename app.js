7486118795b516b54689a36fa6236710270732f7
Przemysław Furtak <przemyslawf@gmail.com>
2018-04-23 12:13:01 +0200

    Update app.js
    Automatically run the application

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 (function (snake) {
 	const button = document.getElementById("start");
+	snake.init();
 	button.addEventListener("click",function(e){
-    	snake.init();	
+    	    snake.init();	
 	},false);
 
 	
